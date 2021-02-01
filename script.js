c650da29af1cb5ec0b61bfcd18eb38e7cfdbe78a
Prakit Wongnuntananon <boatnakrub_@hotmail.com>
2021-02-02 00:21:38 +0700

    [Update] burger bar

--- script.js
+++ script.js
@@ -1,2 +1,11 @@
-(() => {
-})();
+const burger = document.querySelector('.burger');
+const navLinks = document.querySelector('.nav-links');
+const closeBtn = document.querySelector('.close-btn');
+
+burger.addEventListener('click', () => {
+    navLinks.classList.toggle('open');
+    closeBtn.classList.remove('hidden');
+});
+closeBtn.addEventListener('click', () => {
+    navLinks.classList.toggle('open');
+})
