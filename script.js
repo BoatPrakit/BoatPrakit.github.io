1efb449686e9607b0e28e5b7d8551accf69fd070
Prakit Wongnuntananon <boatnakrub_@hotmail.com>
2021-02-04 00:02:53 +0700

    [Update] animation

--- script.js
+++ script.js
@@ -2,8 +2,12 @@ const aboutmeBtn = document.getElementById('about-me')
 const resumeBtn = document.getElementById('resume');
 const projectBtn = document.getElementById('projects');
 const closeBtn = document.querySelector('.close-btn');
+const mainPage = document.getElementById('main-page');
+const resumePage = document.getElementById('resume-page');
+const projectsPage = document.getElementById('projects-page');
 const body = document.body;
-const links = [aboutmeBtn,resumeBtn,projectBtn];
+const linkBtns = [aboutmeBtn,resumeBtn,projectBtn];
+const pages = [mainPage,resumePage,projectsPage];
 const currentBtn = [true,false,false];
 let prevIndex = 0;
 function burger(){
@@ -21,17 +25,28 @@ function updateCurrentBtn(index){
     if(prevIndex == index) return;
     currentBtn[prevIndex] = false;
     currentBtn[index] = true;
-    links[prevIndex].classList.remove('active');
-    links[index].classList.add('active');
+    linkBtns[prevIndex].classList.remove('active');
+    pages[prevIndex].classList.add('hidden');
+    pages[prevIndex].classList.add('xl:hidden');
+    pages[prevIndex].classList.remove('animationFadeIn');
+    linkBtns[index].classList.add('active');
+    pages[index].classList.remove('hidden');
+    pages[index].classList.remove('xl:hidden');
+    pages[index].classList.add('xl:flex');
     prevIndex = index;
 }
 function showSelectedContent(index,isRemove,className){
-    updateCurrentBtn(index);
-    if(isRemove) body.classList.remove(className);
-    else body.classList.add(className);
-    if(innerWidth < 1024) {
-        closeBtn.click();    
-    }
+    setTimeout(() => {
+        updateCurrentBtn(index);
+        if(isRemove) body.classList.remove(className);
+        else {
+            body.classList.add(className);
+        }
+        if(innerWidth < 1024) {
+            closeBtn.click();    
+        }
+        pages[index].classList.add('animationFadeIn');
+    },200);
 }
 function run(){
     burger();
